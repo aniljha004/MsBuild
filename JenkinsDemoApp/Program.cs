dad13a285f505b0051f0113db0cb5e11299acee8
Anil jha <Aniljha004@gmail.com>
2022-01-04 22:16:45 +0530

    Add project files.

---
+++ JenkinsDemoApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JenkinsDemoApp
+{
+    public class Program
+    {
+       public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
